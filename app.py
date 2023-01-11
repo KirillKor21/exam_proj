38ce56f9e633723383ad495d995c45f3cbf4a38f
KirillKor21 <pro100kirik921@mail.ru>
2023-01-11 10:21:58 +0300

    Функция процентов

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:secret@192.168.31.92:5432/bank_db"
+
+db.init_app(app)
+
+
+class Tbank(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), nullable=False)
+    token = db.Column(db.String(64), nullable=False)
+    full_sum = db.Column(db.Integer)
+    overdraft_limit = db.Column(db.Integer)
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
@@ -10,6 +29,15 @@ def index():
 @app.route("/main")
 def main():
     return render_template("main.html")
+
+
+def percent(full_sum):
+    if int(full_sum) > 1000000:
+        perc = 9
+    else:
+        perc = 10 - int(full_sum) * 0.000001
+
+    return perc
 
 
 app.run(host="0.0.0.0")
